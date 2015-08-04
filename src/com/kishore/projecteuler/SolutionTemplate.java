71f90d3fa92197bd227bf5582097aaaac5dc5463
Kishore Narendran <kishore.narendran09@gmail.com>
2015-08-04 23:02:48 +0700

    Added solution template

---
+++ src/com/kishore/projecteuler/SolutionTemplate.java
@@ -0,0 +1,16 @@
+package com.kishore.projecteuler;
+import java.util.Scanner;
+/**
+ * Created by kishorenarendran on 04/08/15.
+ */
+public class SolutionTemplate {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int i = 0; i < t; i++) {
+            int n = in.nextInt();
+        }
+    }
+
+}
